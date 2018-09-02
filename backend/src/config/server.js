688d4c9cb0f1b42ae04d67790a0331849aface64
Unknown <alequis01@gmail.com>
2018-09-02 13:19:50 -0300

    Configure server and database files

---
+++ backend/src/config/server.js
@@ -0,0 +1,19 @@
+const port = 3003
+
+// realiza o parser do corpo da requisicao recebida (formulario, json)
+const bodyParser = require('body-parser')
+
+// servidor web que roda em cima do node
+const express = require('express')
+
+// cria instancia do express
+const server = express()
+
+// sempre que uma requisao usando o padrao urlencoded e json,
+// o parser sera feito pelo bodyParser
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
+})

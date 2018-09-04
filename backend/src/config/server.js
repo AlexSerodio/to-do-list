be91a57347098d1e200b0e87313e1656a16e23f7
Unknown <alequis01@gmail.com>
2018-09-03 23:26:59 -0300

    Finish backend configuration

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,18 +2,20 @@ const port = 3003
 
 // realiza o parser do corpo da requisicao recebida (formulario, json)
 const bodyParser = require('body-parser')
-
 // servidor web que roda em cima do node
 const express = require('express')
-
 // cria instancia do express
 const server = express()
+const allowCors = require('./cors')
 
 // sempre que uma requisao usando o padrao urlencoded e json,
 // o parser sera feito pelo bodyParser
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server

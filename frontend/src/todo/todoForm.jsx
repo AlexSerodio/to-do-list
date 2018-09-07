96525c3ae85fb60ba79b502ebbc674c4bf9409f9
Unknown <alequis01@gmail.com>
2018-09-07 01:17:15 -0300

    Add refresh, add and remove functions and list of tasks

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,11 +5,15 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div>
         <Grid cols='12 10 9'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input id='description' className='form-control' 
+                placeholder='Adicione uma tarefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
 
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )

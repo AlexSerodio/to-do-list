45e92e9f41a833fbe95dfb0362d957c5ccb18e6e
Alequis <alequis01@gmail.com>
2018-09-09 19:40:15 -0300

    Add search function and change todoForm to class component

--- frontend-redux/src/todo/todoReducer.js
+++ frontend-redux/src/todo/todoReducer.js
@@ -19,6 +19,8 @@ export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case 'DESCRIPTION_CHANGED':
             return { ...state, fieldContent: action.payload}
+        case 'TODO_SEARCHED':
+            return { ...state, taskList: action.payload.data }
         default:
             return state
     }
